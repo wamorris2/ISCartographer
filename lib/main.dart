4655e8274acd9c19ad870f9225ed386b18513fed
pablo <fribarz123@gmail.com>
2023-04-01 12:52:46 -0400

    Added drop down box for selecting near or in room

--- lib/main.dart
+++ lib/main.dart
@@ -50,28 +50,13 @@ class MyHomePage extends StatefulWidget {
 
   final String title;
 
-  final TextStyle textStyle = const TextStyle(
-    fontSize: 20
-  );
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  final String _room_number = "9999";
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String roomNumber = "9999";
+  String locationType = "near room";
 
   @override
   Widget build(BuildContext context) {
@@ -90,51 +75,32 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times lol:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            DropdownButton<String>(
+              value: "near room",
+              items: const <DropdownMenuItem<String>>[
+                DropdownMenuItem(value: "near room", child: Text("near room")),
+                DropdownMenuItem(value: "in room", child: Text("in room"))
+              ],
+              onChanged: (String? value) {
+                setState(() {
+                  locationType = value ?? "near room";
+                });
+              }
             ),
             TextFormField(
-              cursorColor: Colors.green,
-              focusNode: FocusNode(),
               controller: TextEditingController(
-                text: _room_number
+                text: roomNumber
               ),
-              style: widget.textStyle,
               autocorrect: false,
-              autofocus: true,
-              textAlign: TextAlign.center,
               maxLength: 4,
               keyboardType: TextInputType.number
 
             )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
